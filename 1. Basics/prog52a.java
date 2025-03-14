dfc00ae197cba2c37412306d46c5e7eaf875b074
kwildspirit <kwildspirit@protonmail.com>
2025-03-14 02:09:53 +0000

    prog52a

---
+++ 1. Basics/prog52a.java
@@ -0,0 +1,18 @@
+public class prog52a {
+    public static void main(String[] args){
+        int len = 143; 
+        int wid = 82;
+
+        int area = len * wid;
+        int perim = 2 * len + 2 * wid;
+
+        System.out.println("area in: " + area);
+        System.out.println("perim in: " + perim); 
+
+    }
+}
+
+/*
+ * area in: 11726
+perim in: 450
+ */
